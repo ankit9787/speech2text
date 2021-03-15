2c4adf4b49ff959321c5383f230ea11f00d767e3
ankit9787 <ank.singh977@gmail.com>
2021-03-15 22:31:32 +0530

    added landing screen for recording voice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 declare const annyang: any;
 
@@ -7,80 +8,109 @@ declare const annyang: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	voiceActiveSectionDisabled: boolean = true;
-	voiceActiveSectionError: boolean = false;
-	voiceActiveSectionSuccess: boolean = false;
-	voiceActiveSectionListening: boolean = false;
-	voiceText: any;
+export class AppComponent implements OnInit {
+  voiceActiveSectionDisabled: boolean = true;
+  voiceActiveSectionError: boolean = false;
+  voiceActiveSectionSuccess: boolean = false;
+  voiceActiveSectionListening: boolean = false;
+  voiceText: any;
+  closeResult: string = '';
+  user: string = "ankit";
+  @ViewChild('content') openModal!:ElementRef;
+  wordCloud: boolean = false;
 
-  constructor(private ngZone: NgZone){}
+  constructor(private ngZone: NgZone, private modalService: NgbModal) {}
 
-	initializeVoiceRecognitionCallback(): void {
-		annyang.addCallback('error', (err: any) => {
-      if(err.error === 'network'){
-        this.voiceText = "Internet is require";
-        annyang.abort();
-        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
-      } else if (this.voiceText === undefined) {
-				this.ngZone.run(() => this.voiceActiveSectionError = true);
-				annyang.abort();
-			}
-		});
-
-		annyang.addCallback('soundstart', () => {
-      this.ngZone.run(() => this.voiceActiveSectionListening = true);
-		});
-
-		annyang.addCallback('end', () => {
-      if (this.voiceText === undefined) {
-        this.ngZone.run(() => this.voiceActiveSectionError = true);
-				annyang.abort();
-			}
-		});
-
-		annyang.addCallback('result', (userSaid: any) => {
-			this.ngZone.run(() => this.voiceActiveSectionError = false);
-
-			let queryText: any = userSaid[0];
-
-			annyang.abort();
-
-      this.voiceText = queryText;
-
-			this.ngZone.run(() => this.voiceActiveSectionListening = false);
-      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
-		});
+	ngOnInit(): void {
+	
 	}
 
-	startVoiceRecognition(): void {
-    this.voiceActiveSectionDisabled = false;
-		this.voiceActiveSectionError = false;
-		this.voiceActiveSectionSuccess = false;
-    this.voiceText = undefined;
-
-		if (annyang) {
-			let commands = {
-				'demo-annyang': () => { }
-			};
-
-			annyang.addCommands(commands);
-
-      this.initializeVoiceRecognitionCallback();
-
-			annyang.start({ autoRestart: false });
-		}
+	ngAfterViewInit(): void {
+		//this.open();
 	}
 
-	closeVoiceRecognition(): void {
-    this.voiceActiveSectionDisabled = true;
-		this.voiceActiveSectionError = false;
-		this.voiceActiveSectionSuccess = false;
-		this.voiceActiveSectionListening = false;
-		this.voiceText = undefined;
+  open() {
+    const modalRef = this.modalService.open(this.openModal, {
+		keyboard: false,
+		backdrop: 'static'
+	});
+    //modalRef.componentInstance.lesson = lesson;
+  }
 
-		if(annyang){
+  dismissModal(){
+	  this.modalService.dismissAll();
+  }
+
+  initializeVoiceRecognitionCallback(): void {
+  	annyang.addCallback('error', (err: any) => {
+    if(err.error === 'network'){
+      this.voiceText = "Internet is require";
       annyang.abort();
-    }
-	}
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+    } else if (this.voiceText === undefined) {
+  			this.ngZone.run(() => this.voiceActiveSectionError = true);
+  			annyang.abort();
+  		}
+  	});
+
+  	annyang.addCallback('soundstart', () => {
+    this.ngZone.run(() => this.voiceActiveSectionListening = true);
+  	});
+
+  	annyang.addCallback('end', () => {
+    if (this.voiceText === undefined) {
+      this.ngZone.run(() => this.voiceActiveSectionError = true);
+  			annyang.abort();
+  		}
+  	});
+
+  	annyang.addCallback('result', (userSaid: any) => {
+  		this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+  		let queryText: any = userSaid[0];
+
+  		annyang.abort();
+
+    this.voiceText = queryText;
+
+  		this.ngZone.run(() => this.voiceActiveSectionListening = false);
+    this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+  	});
+  }
+
+  startVoiceRecognition(): void {
+  this.voiceActiveSectionDisabled = false;
+  	this.voiceActiveSectionError = false;
+  	this.voiceActiveSectionSuccess = false;
+  this.voiceText = undefined;
+
+  	if (annyang) {
+  		let commands = {
+  			'demo-annyang': () => { }
+  		};
+
+  		annyang.addCommands(commands);
+
+    this.initializeVoiceRecognitionCallback();
+
+  		annyang.start({ autoRestart: false });
+  	}
+  }
+
+  closeVoiceRecognition(): void {
+  this.voiceActiveSectionDisabled = true;
+  	this.voiceActiveSectionError = false;
+  	this.voiceActiveSectionSuccess = false;
+  	this.voiceActiveSectionListening = false;
+  	this.voiceText = undefined;
+
+  	if(annyang){
+    annyang.abort();
+  }
+  }
+
+  showWordCloud(){
+	  this.wordCloud = true;
+  }
+
 }
